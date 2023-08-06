8afab798bfb5cf472a802a80946743914f285f6e
Mariana Moraes <mariana@siasp.com.br>
2023-08-06 12:02:33 -0300

    Documentando funcionalidades da aplicação com swagger

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Repository, getRepository } from "typeorm";
+import { Repository } from "typeorm";
 
 import { AppDataSource } from "../../../../database";
 import { Category } from "../../entities/Category";
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import "reflect-metadata";
 import { AppDataSource } from "./database";
 import "./shared/container";
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 AppDataSource.initialize()
