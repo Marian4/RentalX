53f0a117e30e19a7afff13c912c114fa7e846447
Mariana Moraes <mariana@siasp.com.br>
2023-09-25 11:30:58 -0300

    Trocando banco para postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,11 @@
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "",
+  type: "postgres",
+  username: "docker",
+  password: "docker123",
   database: process.env.NODE_ENV === "test" ? "rentalx_test" : "rentalx",
-  port: 3306,
+  port: 5432,
   entities: ["src/modules/**/entities/*.ts"],
   migrations: ["src/database/migrations/*.ts"],
 });
